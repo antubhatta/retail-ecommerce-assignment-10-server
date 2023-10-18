f7cac2ee677828197ab26a3283ba340d4c122d52
antubhatta <antubhatta09@gmail.com>
2023-10-19 00:15:17 +0600

    post method added successfully

--- index.js
+++ index.js
@@ -31,12 +31,20 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    app.post('/products',async(req,res)=>{
+        const newProduct= req.body 
+        console.log("added a new product",newProduct)    
+        // const result = await coffeeCollection.insertOne(newCoffee);
+        // res.send(result)
+
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
